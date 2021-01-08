40bb317e07e05bcd9eab586bba793b8f6d078b28
Eric Campbell <1234goal@gmail.com>
2021-01-08 14:31:59 -0500

    finish wiring epik frontend to xfst

--- app/Main.hs
+++ app/Main.hs
@@ -16,135 +16,28 @@ import GuardedStrings
 import Epik
 import Lexer
 import Parser
+import FstGen
 
--- h = TVar "H"
--- t = TVar "T"
+data Mode = Kat | GS | Auto | Fst deriving (Eq,Show,Ord)
 
--- ht_no_change_program = ifElse h (KTest h) $ ifElse t (KTest t) (KTest TFalse)
-
--- htAlpha = Set.fromList ["H", "T"]
--- htMutex = (h `TAnd` TNeg t) `TOr` (t `TAnd` TNeg h)
-
--- -- atomic actions
--- heads = "H"
--- tails = "T"
--- -- agents
--- alice = "alice"
--- bob = "bob"
-
--- -- helper
--- test_prop ht = KTest $ TVar ht
-
--- -- Programs
--- public_announce :: AtomicTest -> AtomicProgram
--- public_announce ht = "public_announce_" ++ ht
-
--- noop :: AtomicProgram
--- noop = "no_operation"
-
--- peeksAt, sneaksALookAt :: Agent -> AtomicTest -> AtomicProgram
--- peeksAt agent ht = agent ++ "_peek_" ++ ht
--- sneaksALookAt agent ht = agent ++ "_sneak_" ++ ht
-
--- -- helper
--- test_prog name ht = (name ht, test_prop ht)
-
-
--- -- Alternative Relation
--- alice_alternative = ("alice" ,
---                      Map.fromList [(public_announce heads, [public_announce heads]),
---                                    (public_announce tails, [public_announce tails]),
---                                    (alice `peeksAt` heads, [alice `peeksAt` heads]),
---                                    (alice `peeksAt` tails, [alice `peeksAt` tails]),
---                                    (alice `sneaksALookAt` heads, [alice `sneaksALookAt` heads]),
---                                    (alice `sneaksALookAt` tails, [alice `sneaksALookAt` heads]),
---                                    (bob `peeksAt` heads, [bob `peeksAt` heads, bob `peeksAt` tails]),
---                                    (bob `peeksAt` tails, [bob `peeksAt` tails, bob `peeksAt` tails]),
---                                    (bob `sneaksALookAt` heads, [noop]),
---                                    (bob `sneaksALookAt` tails, [noop])
---                                   ])
-
--- bob_alternative = ("bob",
---                    Map.fromList [(public_announce heads, [public_announce heads]),
---                                  (public_announce tails, [public_announce tails]),
---                                  (alice `peeksAt` heads, [alice `peeksAt` tails, alice `peeksAt` heads]),
---                                  (alice `peeksAt` tails, [alice `peeksAt` heads, alice `peeksAt` heads]),
---                                  (bob `peeksAt` heads, [bob `peeksAt` heads]),
---                                  (bob `peeksAt` tails, [bob `peeksAt` tails]),
---                                  (alice `sneaksALookAt` heads, [noop]),
---                                  (alice `sneaksALookAt` tails, [noop]),
---                                  (bob `sneaksALookAt` heads, [bob `sneaksALookAt` heads]),
---                                  (bob `sneaksALookAt` tails, [bob `sneaksALookAt` tails])
---                                  ])
-
--- bob_sees_heads =
---   qplus(QIdent (public_announce heads)
---         `QUnion` QIdent (bob `peeksAt` heads)
---         `QUnion` QIdent (bob `sneaksALookAt` heads))
-
-
--- -- A Hypothesis for the knowledge query.
--- -- [know x t] is a literal translation of K_x(t) from the writeup
--- knows :: Agent -> Query -> Query
--- knows x query = QComplement (QIdent x `QApply` (QComplement query))
-
-
--- -- public_announce_H + public_announce_T
--- public_announce_query =
---   foldr1 (QUnion) $
---   map (QIdent . public_announce) [heads,tails]
-
--- ever query = (QStar QAll) `QConcat` query `QConcat`(QStar QAll)
-
--- prog = Program
---   (Set.fromList [heads, tails]) -- alphabet
---   [htMutex]  -- assertion
---   [(noop, Nop),
---    test_prog public_announce heads, ----- > H; public_announce_H; H
---    test_prog public_announce tails, ----- > T; public_announce_T; T
---    test_prog (peeksAt alice) heads, 
---    test_prog (peeksAt alice) tails,
---    test_prog (peeksAt bob) heads,
---    test_prog (peeksAt bob) tails,
---    test_prog (sneaksALookAt alice) heads,
---    test_prog (sneaksALookAt bob) tails,
---    test_prog (sneaksALookAt bob) heads,
---    test_prog (sneaksALookAt bob) tails
---   ]
---   [bob_alternative, alice_alternative]
---   [("EverAnnounces?", ever public_announce_query),
---    ("bob saw heads", bob_sees_heads),
---    ("What does Alice know about Bob (who saw heads)?", alice `knows` (bob_sees_heads))
---   ]
-  
--- hd [] = undefined
--- hd (x:_) = x
-
--- getAuto (QAuto q) = q
--- getAuto _ = undefined
-
--- autoqueries = foldr (\(_,x) rst -> case x of
---                                   QAuto a -> a : rst
---                                   _ -> rst
---                         ) [] (runQueries prog)
--- -- ctx = compileDecls prog
--- query =
---   let Just a = lookupAction (actionsc ctx) "public_announce" in
---     katFromAction a
-
-parseArgs :: [String] -> (String, Bool, Int, Bool)
+parseArgs :: [String] -> (String, Mode, Int)
 parseArgs [] = error "insufficient arguments!"
-parseArgs ("kat":f:_)  = (f, True, (-1), False)
-parseArgs ("gs":n:f:_) = (f, False, read n, False)
-parseArgs ("auto":n:f:_) = (f, False, read n, True)
-parseArgs (n:f:_) = (f, False, read n, False) -- If no command given, default to GS model
+parseArgs ("kat":f:_)  = (f, Kat, (-1))
+parseArgs ("gs":n:f:_) = (f, GS, read n)
+parseArgs ("auto":n:f:_) = (f, Auto, read n)
+parseArgs ("fst":f:_) = (f, Fst, (-1))
+parseArgs (n:f:_) = (f, GS, read n) -- If no command given, default to GS model
 parseArgs args = error ("Unrecognized arguments " ++ intercalate " " args)
-  
+
+process :: String -> (Declarations -> String) -> IO ()
+process file fun = readFile file >>= putStrLn . fun . parse
+
 main :: IO ()
 main = do
   args <- getArgs
-  let (file, kat, num, auto) = parseArgs args
-  contents <- readFile file
-  putStrLn $ if kat
-             then showKatTerms $ parse contents
-             else showQueryResults num auto $ parse contents
+  let (file, mode, num) = parseArgs args
+  case mode of
+    Kat  -> process file showKatTerms
+    GS   -> process file $ showQueryResults num False
+    Auto -> error "No automaton model is available yet"
+    Fst  -> process file gen_fst
